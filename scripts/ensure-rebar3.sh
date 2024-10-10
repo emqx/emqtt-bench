b5bd14b0bf4ff1872d5ac0285ba89f2fecf710c9
Ivan Dyachkov <dev@dyachkov.org>
2024-10-10 17:46:29 +0200

    chore: support OTP 27

--- scripts/ensure-rebar3.sh
+++ scripts/ensure-rebar3.sh
@@ -9,11 +9,14 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
 OTP_VSN=$(erl -noshell -eval 'io:format("~s~n", [erlang:system_info(otp_release)]), halt().')
 case ${OTP_VSN} in
-    25*)
-        VERSION="3.19.0-emqx-9"
+    27*)
+        VERSION="3.20.0-emqx-3"
         ;;
     26*)
         VERSION="3.20.0-emqx-1"
+        ;;
+    25*)
+        VERSION="3.19.0-emqx-9"
         ;;
     *)
         echo "Unsupported Erlang/OTP version $OTP_VSN"
